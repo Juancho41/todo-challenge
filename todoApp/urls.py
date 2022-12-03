b433257022f5c00089d2890cc204a4fa122c6f41
JuanC <juan_corica@hotmail.com>
2022-12-03 03:45:16 -0300

    se agregan serializadores, y se crean urls y vistas para ver lista de tareas, ver detalles, crear, borrar y modificar tareas

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
+    path('lista-tareas/', views.listaTareas, name='lista-tareas'),
+    path('detalles-tarea/<str:pk>/', views.detallesTarea, name='detalles-tarea'),
+    path('crear-tarea/', views.crearTarea, name='crear-tarea'),
+    path('modificar-tarea/<str:pk>/', views.modificarTarea, name='modificar-tarea'),
+    path('borrar-tarea/<str:pk>/', views.borrarTarea, name='borrar-tarea'),
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TareaSerializer
+from .models import Tarea
 
 # Create your views here.
 
@@ -15,3 +17,37 @@ def apiOverview(request):
         'Borrar': '/borrar-tarea/<str:pk>/',
         }
     return Response (api_urls)
+
+
+@api_view(['GET'])
+def listaTareas(request):
+    tareas = Tarea.objects.all()
+    serializer = TareaSerializer(tareas, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def detallesTarea(request, pk):
+    tareas = Tarea.objects.get(id=pk)
+    serializer = TareaSerializer(tareas, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def crearTarea(request):
+    serializer = TareaSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def modificarTarea(request, pk):
+    tarea = Tarea.objects.get(id=pk)
+    serializer = TareaSerializer(instance=tarea, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def borrarTarea(request, pk):
+    tarea = Tarea.objects.get(id=pk)
+    tarea.delete()
+    return Response('Tarea eliminada!')
